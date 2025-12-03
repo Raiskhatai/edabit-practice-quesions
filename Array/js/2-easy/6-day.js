024c67b1da6ff79e24ac0d342dc6eac6dce2356b
Rais Khatai <raiskhatai@gmail.com>
2025-12-04 00:40:49 +0530

    6th day

--- Array/js/2-easy/6-day.js
+++ Array/js/2-easy/6-day.js
@@ -181,14 +181,61 @@
 // 37 => Mirror Array
 // Given an integer array, transform that array into a mirror.
 
-function mirror(arr) {
-  let rev_arr = [...arr];
-  rev_arr.reverse();
-  rev_arr = rev_arr.slice(1);
-  arr = arr.concat(rev_arr);
-  return arr;
-}
+// function mirror(arr) {
+//   let rev_arr = [...arr];
+//   rev_arr.reverse();
+//   rev_arr = rev_arr.slice(1);
+//   arr = arr.concat(rev_arr);
+//   return arr;
+// }
 
-console.log(mirror([0, 2, 4, 6])); // [0, 2, 4, 6, 4, 2, 0]
-console.log(mirror([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5, 4, 3, 2, 1]
-console.log(mirror([3, 5, 6, 7, 8])); // [3, 5, 6, 7, 8, 7, 6, 5, 3]
+// function mirror(arr) {
+//   let newarr = [...arr];
+//   let j = arr.length;
+//   for (let i = arr.length - 2; i >= 0; i--) {
+//     newarr[j] = arr[i];
+//     j++;
+//   }
+//   return newarr;
+// }
+
+// console.log(mirror([0, 2, 4, 6])); // [0, 2, 4, 6, 4, 2, 0]
+// console.log(mirror([1, 2, 3, 4, 5])); // [1, 2, 3, 4, 5, 4, 3, 2, 1]
+// console.log(mirror([3, 5, 6, 7, 8])); // [3, 5, 6, 7, 8, 7, 6, 5, 3]
+
+// 38 => Odd Up, Even Down — N Times
+// Create a function that performs an even-odd transform to an array, n times. Each even-odd transformation:
+
+// Adds two (+2) to each odd integer.
+// Subtracts two (-2) from each even integer.
+
+// function evenOddTransform(arr, val) {
+//   let up_val = val * 2;
+//   let newarr = [];
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0) {
+//       newarr[i] = arr[i] - up_val;
+//     } else {
+//       newarr[i] = arr[i] + up_val;
+//     }
+//   }
+//   return newarr;
+// }
+
+// function evenOddTransform(arr, val) {
+//   let up_val = val * 2;
+//   let newarr = arr.map((item) => {
+//     if (item % 2 == 0) {
+//       return item - up_val;
+//     } else {
+//       return item + up_val;
+//     }
+//   });
+//   return newarr;
+// }
+
+// console.log(evenOddTransform([3, 4, 9], 3)); // [9, -2, 15]
+// // Since [3, 4, 9] => [5, 2, 11] => [7, 0, 13] => [9, -2, 15]
+
+// console.log(evenOddTransform([0, 0, 0], 10)); // [-20, -20, -20]
+// console.log(evenOddTransform([1, 2, 3], 1)); // [3, 0, 5]
