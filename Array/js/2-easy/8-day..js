ea960d076442bc171792da8abb9b515d984b90ff
Rais Khatai <raiskhatai@gmail.com>
2025-12-06 17:03:10 +0530

    9-day

--- Array/js/2-easy/8-day..js
+++ Array/js/2-easy/8-day..js
@@ -141,4 +141,169 @@
 // console.log(countPosSumNeg([91, -4, 80, -73, -28])); // [2, -105]
 // console.log(countPosSumNeg([])); // []
 
+// 47 => Inclusive Array Ranges
+// Write a function that, given the start startNum and end endNum values, return an array containing all the numbers inclusive to that range. See examples below.
+
+// function inclusiveArray(startNum, endNum) {
+//   let arr = [];
+//   let j = 0;
+//   if (startNum > endNum) {
+//     arr[0] = startNum;
+//     return arr;
+//   } else {
+//     for (let i = startNum; i <= endNum; i++) {
+//       arr[j] = i;
+//       j++;
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(inclusiveArray(1, 5)); // [1, 2, 3, 4, 5]
+// console.log(inclusiveArray(2, 8)); // [2, 3, 4, 5, 6, 7, 8]
+// console.log(inclusiveArray(10, 20)); // [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+// console.log(inclusiveArray(17, 5)); // [17]
+
+// 48 => Sum of Found Indexes
+// Create a function which takes in an array of numbers and a number to find. Return the sum of every index in the array which matches the chosen number.
+
+// function sumFoundIndexes(arr, val) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == val) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+
+// function sumFoundIndexes(arr, val) {
+//   let sum = arr.reduce((a, v, i) => {
+//     if (v == val) {
+//       return a + i;
+//     } else {
+//       return a;
+//     }
+//   }, 0);
+//   return sum;
+// }
+
+// console.log(sumFoundIndexes([0, 3, 3, 0, 0, 3], 3)); // 8
+// // // The number 3 was found at indexes 1, 2 and 5.
+// // // 8 = 1 + 2 + 5
+
+// console.log(sumFoundIndexes([1, 2, 3, 4, 5, 6], 3)); // 2
+// console.log(sumFoundIndexes([100, 100, 100, 100, 100], 100)); // 10
+// console.log(sumFoundIndexes([5, 10, 15, 20], 2)); // 0
+
+// 49 => Sum of the Items in an Array
+// Create a function that takes an array and returns the sum of all items in the array.
+
+// function sumArray(arr) {
+//   arr = arr.flat(Infinity);
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+// function sumArray(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (Array.isArray(arr[i])) {
+//       sum += sumArray(arr[i]);
+//     } else {
+//       sum += arr[i];
+//     }
+//   }
+//   return sum;
+// }
+
+// console.log(sumArray([1, 2, 3])); // 6
+// // 1 + 2 + 3 = 6
+// console.log(sumArray([1, [2, [1]], 3])); // 7
+// // 1 + 2 + 1 + 3 = 7
+
+// 50 => Sum of all Evens in a Matrix
+// Create a function that returns the sum of all even elements in a 2D matrix.
+
+// function sumOfEvens(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let k = 0; k < arr[i].length; k++) {
+//       if (arr[i][k] % 2 == 0) {
+//         sum += arr[i][k];
+//       }
+//     }
+//   }
+//   return sum;
+// }
+
+// function sumOfEvens(arr) {
+//   let sum = 0;
+//   arr.forEach((Element) => {
+//     return Element.forEach((item) => {
+//       if (item % 2 == 0) {
+//         sum += item;
+//       }
+//     });
+//   });
+//   return sum;
+// }
+
+// console.log(
+//   sumOfEvens([
+//     [1, 0, 2],
+//     [5, 5, 7],
+//     [9, 4, 3],
+//   ])
+// ); //6
+
+// // 2 + 4 = 6
+
+// console.log(
+//   sumOfEvens([
+//     [1, 1],
+//     [1, 1],
+//   ])
+// ); //0
+
+// console.log(
+//   sumOfEvens([
+//     [42, 9],
+//     [16, 8],
+//   ])
+// ); //66
+
+// console.log(sumOfEvens([[], [], []])); //0
+
+// 51 => Mini Peaks
+// Write a function that returns all the elements in an array that are strictly greater than their adjacent left and right neighbors.
+
+// function miniPeaks(arr) {
+//   const ret_arr = [];
+//   let j = 0;
+//   for (let i = 1; i < arr.length - 1; i++) {
+//     if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1]) {
+//       ret_arr[j] = arr[i];
+//       j++;
+//     }
+//   }
+//   return ret_arr;
+// }
+
+// function miniPeaks(arr) {
+//   let ret_arr = arr.filter((item, i, inner) => {
+//     if (item > inner[i + 1] && item > inner[i - 1]) {
+//       return item;
+//     }
+//   });
+//   return ret_arr;
+// }
+
+// console.log(miniPeaks([4, 5, 2, 1, 4, 9, 7, 2])); // [5, 9]
+// // 5 has neighbours 4 and 2, both are less than 5.
+// console.log(miniPeaks([1, 2, 1, 1, 3, 2, 5, 4, 4])); // [2, 3, 5]
+// console.log(miniPeaks([1, 2, 3, 4, 5, 6])); // []
 
---
+++ Array/js/2-easy/9-day.js
@@ -0,0 +1,244 @@
+// 52 => Find Unique Character Strings
+// Create a function that returns only strings with unique characters.
+
+// function filterUnique(arr) {}
+
+// console.log(filterUnique(["abb", "abc", "abcdb", "aea", "bbb"])); // ["abc"]
+// // "b" occurs in "abb" more than once, "b" occurs in "abcdb" more than once, etc.
+// console.log(filterUnique(["88", "999", "989", "9988", "9898"])); // []
+// console.log(filterUnique(["ABCDE", "DDEB", "BED", "CCA", "BAC"])); // ["ABCDE", "BED", "BAC"]
+
+// 53 => Chocolate Dilemma
+// Two sisters are eating chocolate, whose pieces are represented as subarrays of [l x w].
+
+// Write a function that returns true if the total area of chocolate is the same for each sister.
+
+// To illustrate:
+
+// testFairness([[4, 3], [2, 4], [1, 2]],
+// [[6, 2], [4, 2], [1, 1], [1, 1]])
+// ➞ true
+
+// Agatha's pieces: [4, 3], [2, 4], [1, 2]
+// Bertha's pieces: [6, 2], [4, 2], [1, 1], [1, 1]
+
+// Total area of Agatha's chocolate
+// 4x3 + 2x4 + 1x2 = 12 + 8 + 2 = 22
+
+// Total area of Bertha's chocolate is:
+// 6x2 + 4x2 + 1x1 + 1x1 = 12 + 8 + 1 + 1 = 22
+
+// function testFairness(arr1, arr2) {
+//   arr1 = arr1.flat(Infinity);
+//   let sum = 0;
+//   for (let i = 0; i < arr1.length; i += 2) {
+//     sum += arr1[i] * arr1[i + 1];
+//   }
+//   arr2 = arr2.flat(Infinity);
+//   let sum2 = 0;
+//   for (let i = 0; i < arr2.length; i += 2) {
+//     sum2 += arr2[i] * arr2[i + 1];
+//   }
+
+//   return sum === sum2 ? true : false;
+// }
+
+// function testFairness(arr1, arr2) {
+//   arr1 = arr1.flat(Infinity);
+//   let sum1 = arr1.reduce((a, v, i, other) => {
+//     return i % 2 == 0 ? a + other[i] * other[i + 1] : a;
+//   }, 0);
+
+//   arr2 = arr2.flat(Infinity);
+//   let sum2 = arr2.reduce((a, v, i, other2) => {
+//     return i % 2 == 0 ? a + other2[i] * other2[i + 1] : a;
+//   }, 0);
+//   return sum1 == sum2 ? true : false;
+// }
+
+// console.log(testFairness([[1, 2], [2, 1]], [[2, 2]]) ) //true
+// console.log(testFairness([[1, 2], [2, 1]], [[2, 2], [4, 4]]) ) //false
+// console.log(testFairness([[2, 2], [2, 2], [2, 2], [2, 2]], [[4, 4]]) ) //true
+// console.log(testFairness([[1, 5], [6, 3], [1, 1]], [[7, 1], [2, 2], [1, 1]]) )// false
+
+// // 54 => Record Temperatures
+// You are given two arrays that each contain data that represents the min and max weather temperatures for each day of the week.
+
+// The records array contains the all-time record low/high temperatures for that day of the week.
+
+// [[record low, record high], ...]
+// The current week array contains the daily low/high temperatures for each day of the current week.
+
+// [[daily low, daily high], ...]
+// A daily high temperature is considered a new record high if it is higher than the record high for that day of the week. A daily low temperature is considered a new record low if it is lower than the record low for that day of the week.
+
+// Compare the daily low/high temperatures of the current week to the record lows/highs and return an array with the updated record temperatures.
+
+// There may be multiple record temperatures in a week.
+// If there are no broken records return the original records array.
+// Example
+
+// function recordTemps(arr1, arr2) {
+//   let newarr = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     let temp = [];
+//     if (arr1[i][0] < arr2[i][0]) {
+//       temp[0] = arr1[i][0];
+//       newarr[i] = temp;
+//     } else {
+//       temp[0] = arr2[i][0];
+//       newarr[i] = temp;
+//     }
+//     if (arr1[i][1] > arr2[i][1]) {
+//       temp[1] = arr1[i][1];
+//       newarr[i] = temp;
+//     } else {
+//       if (arr1[i][1] < arr2[i][1]) {
+//         temp[1] = arr1[i][1];
+//         newarr[i] = temp;
+//       }
+//     }
+//   }
+//   return newarr;
+// }
+
+// //             sun       mon      tues       wed      thur      fri       sat
+// console.log(
+//   recordTemps([[34, 82],[24, 82],[20, 89],[5, 88],[9, 88],[26, 89],[27, 83],],
+//     [[44, 72],[19, 70],[40, 69],[39, 68],[33, 64],[36, 70],[38, 69],])
+// );
+
+//           [[34, 82], [19, 82], [20, 89], [5, 88], [9, 88], [26, 89], [27, 83]]
+// The previous record low for Monday was 24. The current week's low for Monday was 19. So 19 replaces 24 as Monday's new record low.
+
+// 55 => Check if One Array is a Subset of Another
+// Array A is contained inside array B if each element in A also exists in B.
+
+// The number of times a number is present doesn't matter. In other words, if we transformed both arrays into sets, A would be a subset of B.
+
+// A = [3, 3, 9, 9, 9, 5]
+// B = [1, 3, 9, 5, 8, 44, 44]
+
+// A_Set = [3, 9, 5]
+// B_Set = [1, 3, 9, 5, 8, 44]
+
+// // A_Set is a subset of B_Set
+// Create a function that determines if the first array is a subset of the second.
+
+// function subset(arr1, arr2) {
+//   arr1 = [...new Set(arr1)];
+//   arr2 = [...new Set(arr2)];
+//   let res = true;
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     for (let k = 0; k < arr2.length; k++) {
+//       if (arr1[i] == arr2[k]) {
+//         res = true;
+//         break;
+//       } else if (arr1[i] != arr2[k] && arr2.length - 1 == k) {
+//         return false;
+//       }
+//     }
+//   }
+//   return res;
+// }
+
+// function subset(arr1, arr2) {
+//   arr1 = [...new Set(arr1)];
+//   arr2 = [...new Set(arr2)];
+//   let res = true;
+
+//   arr1.forEach((item) => {
+//     if (!arr2.includes(item)) {
+//       res = false;
+//     }
+//   });
+//   return res;
+// }
+
+// function subset(arr1, arr2) {
+//   arr1 = [...new Set(arr1)];
+//   arr2 = [...new Set(arr2)];
+//   let res = true;
+
+//   return arr1.every((item) => arr2.includes(item));
+// }
+
+// console.log(subset([1, 3], [1, 3, 3, 5])); // true
+// console.log(subset([4, 8, 7], [7, 4, 4, 4, 9, 8])); // true
+// console.log(subset([1, 3], [1, 33])); // false
+// console.log(subset([1, 3, 10], [10, 8, 8, 8])); // false
+
+// 56 => Even Index Elements in Array
+// Create a function that takes an array of integers and returns the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+// For example:
+
+// [2, 3, 4, 5] ➞ 30
+// (2 + 4) * 5 ➞ 30
+
+// [1, 4, 5, 6, 7, 2, 3] ➞ 48
+// (1 + 5 + 7 + 3) * 3 ➞ 48
+
+// function evenLast(arr) {
+//   if (arr.length == 0) {
+//     return 0;
+//   }
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i += 2) {
+//     sum += arr[i];
+//   }
+//   let res = sum * arr[arr.length - 1];
+//   return res;
+// }
+
+// function evenLast(arr) {
+//   if (arr.length == 0) {
+//     return 0;
+//   }
+
+//   let sum = arr.reduce((a, v, i) => {
+//     if (i % 2 == 0) {
+//       return a + v;
+//     } else {
+//       return a;
+//     }
+//   }, 0);
+//   let res = sum * arr[arr.length - 1];
+//   return res;
+// }
+
+// console.log(evenLast([])); // 0
+// console.log(evenLast([1, 3, 3, 1, 10])); // 140
+// console.log(evenLast([-11, 3, 3, 1, 10])); // 20
+
+// 57 => Back to Home?
+// Mubashir has started his journey from home. Given a string of directions (N=North, W=West, S=South, E=East), he will walk for one minute in each direction. Determine whether a set of directions will lead him back to the starting position or not.
+
+// function backToHome(directions) {
+//   let e = 0,
+//     n = 0,
+//     w = 0,
+//     s = 0;
+//   for (let i = 0; i < directions.length; i++) {
+//     if (directions[i] == "E") {
+//       e++;
+//     } else if (directions[i] == "N") {
+//       n++;
+//     } else if (directions[i] == "S") {
+//       s++;
+//     } else if (directions[i] == "W") {
+//       w++;
+//     }
+//   }
+//   if (n == s && e == w) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+console.log(backToHome("EEWE")); // false
+console.log(backToHome("NENESSWW")); // true
+console.log(backToHome("NEESSW")); // false
