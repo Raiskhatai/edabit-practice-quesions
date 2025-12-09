c7331330384b1bd7487abc3f2cc7a864f0304f5a
Rais Khatai <raiskhatai@gmail.com>
2025-12-09 23:17:05 +0530

    60th question complete

--- Array/js/2-easy/12-day.js
+++ Array/js/2-easy/12-day.js
@@ -1,4 +1,4 @@
-// 58 Sum of Odd and Even Numbers
+// 58 => Sum of Odd and Even Numbers
 // Write a function that takes an array of numbers and returns an array with two elements:
 
 // The first element should be the sum of all even numbers in the array.
@@ -28,3 +28,83 @@
 
 // console.log(sumOddAndEven([-1, -2, -3, -4, -5, -6])); // [-12, -9]
 // console.log(sumOddAndEven([0, 0])); // [0, 0]
+
+// 59 => No Hidden Fees
+// Given an array of prices prices and a "supposed" total t, return true if there is a hidden fee added to the total (i.e. the total is greater than the sum of prices), otherwise return false.
+
+// function hasHiddenFee(arr, val) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     let temp = +arr[i].slice(1);
+//     sum += temp;
+//   }
+//   val = +val.slice(1);
+//   return val > sum ? true : false;
+// }
+
+// function hasHiddenFee(arr, val) {
+//   let sum = arr.reduce((a, v) => a + Number(v.slice(1)), 0);
+//   val = +val.slice(1);
+//   return sum < val ? true : false;
+// }
+
+// console.log(hasHiddenFee(["$2", "$4", "$1", "$8"], "$15")); // false
+// console.log(hasHiddenFee(["$1", "$2", "$3"], "$6")); // false
+// console.log(hasHiddenFee(["$1"], "$4")); // true
+
+// 60 Count Letters in a Word Search
+// Create a function that counts the number of times a particular letter shows up in the word search.
+
+// function letterCounter(arr, letter) {
+//   let counter = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let k = 0; k < arr[i].length; k++) {
+//       if (arr[i][k] == letter) {
+//         counter++;
+//       }
+//     }
+//   }
+//   return counter;
+// }
+
+// function letterCounter(arr, letter) {
+//   let counter = 0;
+//   arr.forEach((Element) => {
+//     Element.forEach((item) => {
+//       if (item == letter) {
+//         counter++;
+//       }
+//     });
+//   });
+//   return counter;
+// }
+
+// console.log(
+//   letterCounter(
+//     [
+//       ["D", "E", "Y", "H", "A", "D"],
+//       ["C", "B", "Z", "Y", "J", "K"],
+//       ["D", "B", "C", "A", "M", "N"],
+//       ["F", "G", "G", "R", "S", "R"],
+//       ["V", "X", "H", "A", "S", "S"],
+//     ],
+//     "D"
+//   )
+// ); // 3
+
+// // "D" shows up 3 times: twice in the first row, once in the third row.
+
+// console.log(
+//   letterCounter(
+//     [
+//       ["D", "E", "Y", "H", "A", "D"],
+//       ["C", "B", "Z", "Y", "J", "K"],
+//       ["D", "B", "C", "A", "M", "N"],
+//       ["F", "G", "G", "R", "S", "R"],
+//       ["V", "X", "H", "A", "S", "S"],
+//     ],
+//     "H"
+//   )
+// ); // 2
+
+
