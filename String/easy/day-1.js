5bb1b0b8c75878a133e20e7ce88ff1d54b2e4bc2
Raiskhatai <raiskhatai@gmail.com>
2026-04-23 23:47:52 +0530

    5th question

---
+++ String/easy/day-1.js
@@ -0,0 +1,55 @@
+// 1 => Return Something to Me!
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+// function giveMeSomething(str) {
+//   retVal = "something " + str;
+//   return retVal;
+// }
+
+// console.log(giveMeSomething("is better than nothing")); // "something is better than nothing"
+// console.log(giveMeSomething("Bob Jane")); // "something Bob Jane"
+// console.log(giveMeSomething("something")); // "something something"
+
+// 2 =>Boolean to String Conversion
+// Create a function that takes a boolean variable flag and returns it as a string.
+
+// function boolToString(flag) {
+//   let retVal = "" + flag;
+//   return retVal;
+// }
+
+// console.log(boolToString(true)); // "true"
+// console.log(boolToString(false)); // "false"
+
+// 3 => Compare Strings by Count of Characters
+// Create a function that takes two strings as arguments and return either true or false depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+
+// function comp(str1, str2) {
+//   return str1.length === str2.length ? true : false;
+// }
+
+// console.log(comp("AB", "CD")); // true
+// console.log(comp("ABC", "DE")); // false
+// console.log(comp("hello", "edabit")); // false
+
+// 4 => Is the String Empty?
+// Create a function that returns true if a string is empty and false otherwise.
+
+// function isEmpty(s) {
+//   return s.length === 0 ? true : false;
+// }
+
+// console.log(isEmpty("")); // true
+// console.log(isEmpty(" ")); // false
+// console.log(isEmpty("a")); // false
+
+// 5 => Concatenate First and Last Name into One String
+// Given two strings, firstName and lastName, return a single string in the format "last, first".
+
+// function concatName(firstName, lastName) {
+//   return lastName + ", " + firstName;
+// }
+
+// console.log(concatName("First", "Last")); // "Last, First"
+// console.log(concatName("John", "Doe")); // "Doe, John"
+// console.log(concatName("Mary", "Jane")); // "Jane, Mary"
